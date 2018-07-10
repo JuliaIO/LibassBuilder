c3404ef72230ceb659c90d2036afd3314c6d9d40
SimonDanisch <sdanisch@gmail.com>
2018-07-10 22:48:52 +0200

    init builder

---
+++ build_tarballs.jl
@@ -0,0 +1,52 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+name = "libass"
+version = v"0.14.0"
+# Collection of sources required to build LibassBuilder
+sources = [
+    "https://github.com/libass/libass/releases/download/0.14.0/libass-0.14.0.tar.xz" =>
+    "881f2382af48aead75b7a0e02e65d88c5ebd369fe46bc77d9270a94aa8fd38a2",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd libass-0.14.0/
+./configure --prefix=$prefix --host=$target --disable-require-system-font-provider
+make -j${ncore}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:i686, :glibc),
+    Linux(:x86_64, :glibc),
+    Linux(:aarch64, :glibc),
+    Linux(:armv7l, :glibc, :eabihf),
+    Linux(:powerpc64le, :glibc),
+    Linux(:i686, :musl),
+    Linux(:x86_64, :musl),
+    Linux(:aarch64, :musl),
+    Linux(:armv7l, :musl, :eabihf),
+    MacOS(:x86_64),
+    FreeBSD(:x86_64),
+    Windows(:i686),
+    Windows(:x86_64)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libass", :libass),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    "https://github.com/Gnimuc/FreeTypeBuilder/releases/download/v2.9/build.jl",
+    "https://github.com/SimonDanisch/FribidiBuilder/releases/download/0.14.0/build_fribidi.v0.14.0.jl",
+    "https://github.com/SimonDanisch/NASMBuilder/releases/download/2.13.3/build_nasm.v2.13.3.jl"
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
