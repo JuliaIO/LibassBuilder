0cf987b71218e261386e0526c175152979c0ac8e
Ian <i.r.butterworth@gmail.com>
2019-05-30 14:26:51 -0400

    fix zlib on freebsd

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -12,6 +12,7 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
+apk add nasm
 cd libass-0.14.0/
 sed -i 's/9.10.3/2.9.1/' configure.ac
 autoreconf
@@ -34,7 +35,7 @@ dependencies = [
     "https://github.com/JuliaGraphics/FreeTypeBuilder/releases/download/v2.9.1-4/build_FreeType2.v2.10.0.jl",
     "https://github.com/SimonDanisch/FribidiBuilder/releases/download/0.14.0/build_fribidi.v0.14.0.jl",
     "https://github.com/JuliaPackaging/Yggdrasil/releases/download/Bzip2-v1.0.6-0/build_Bzip2.v1.0.6.jl",
-    "https://github.com/bicycle1885/ZlibBuilder/releases/download/v1.0.4/build_Zlib.v1.2.11.jl"
+    "https://github.com/ianshmean/ZlibBuilder/releases/download/v1.2.11/build_Zlib.v1.2.11.jl"
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
