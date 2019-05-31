ad63940092c5a2323c97a2ae124e747a4df733ee
Ian <i.r.butterworth@gmail.com>
2019-05-31 15:05:19 -0400

    fix for nasm & macos

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -12,9 +12,14 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
+apk add nasm
 cd libass-0.14.0/
 sed -i 's/9.10.3/2.9.1/' configure.ac
 autoreconf
+# Grumble-grumble apple grumble-grumble broken linkers...
+if [[ ${target} == *-apple-* ]]; then
+    export AR=/opt/${target}/bin/ar
+fi
 ./configure --prefix=$prefix --host=$target --disable-require-system-font-provider
 make -j${ncore}
 make install
